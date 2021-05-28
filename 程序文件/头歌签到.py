62f0a0fd1b7853be66d4176f988ac998b30c832c
Mr.X <58155247+ZhReimu@users.noreply.github.com>
2021-05-29 07:51:21 +0800

    Update 头歌签到.py

--- 程序文件/头歌签到.py
+++ 程序文件/头歌签到.py
@@ -1,8 +1,8 @@
 import requests
 
 cookies = {
-    'autologin_trustie': '4aa262a163d74699bf16830ad611107f2efad2e0',
-    '_educoder_session': '06b564a3d652a99cf8af4e7c62eefd2c',
+    'autologin_trustie': '07dfda93a037d24905c284eacbd8e21b9ebe5b70',
+    '_educoder_session': 'ff322b1ce457fde9a8379086630ba9ac',
 }
 
 headers = {
@@ -21,4 +21,4 @@ headers = {
 }
 
 response = requests.post('https://data.educoder.net/api/users/attendance.json', headers=headers, cookies=cookies)
-print(response.text)
+pront(response.json())
